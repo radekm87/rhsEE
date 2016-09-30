e25b3aacfeef2a25e42daf77178b0e40d79fb54a
rmorawski <radoslaw.morawski@opi.org.pl>
2016-09-30 16:06:24 +0200

    JPA - gówno.  złe utworzenie artifactu w IDEI. Folder wrzucany do np Glassfisha musi miec plik persistence.xml w: out/NAZWA/WEB-INF/classes/META-INF/persistence.xml

--- webServiceRS/src/main/java/pl/radmit/javaee/domain/Product.java
+++ webServiceRS/src/main/java/pl/radmit/javaee/domain/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -11,11 +12,13 @@ import javax.validation.constraints.NotNull;
  * Created by rmorawski on 29.09.16.
  */
 @Entity
-@Table
+//@Table(name = "STUDENT")
+@Table(name = "Product")
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PROD_SEQ")
+    @SequenceGenerator(name = "PROD_SEQ", sequenceName = "PRODUCT3_SEQ", allocationSize = 1)
     private Long id;
 
     @NotNull
--- webServiceRS/src/main/java/pl/radmit/javaee/domain/ProductEntityManager.java
+++ webServiceRS/src/main/java/pl/radmit/javaee/domain/ProductEntityManager.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+
 /**
  * Created by rmorawski on 29.09.16.
  */
@@ -11,16 +12,24 @@ public class ProductEntityManager {
 
 
     public void addProduct() {
+//        UserTransaction transaction = (UserTransaction)new InitialContext().lookup("java:comp/UserTransaction");
+//        transaction.begin();
+//        EntityManager em = getEntityManager();
+//        Employee employee = em.find(Employee.class, id);
+//        employee.setSalary(employee.getSalary() + 1000);
+//        transaction.commit();
+
+
 //        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("myPU");
-        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("statCreateTablesJTA");
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("example");
 
         EntityManager entitymanager = emfactory.createEntityManager();
-        entitymanager.getTransaction().begin();
+//        entitymanager.getTransaction().begin();
 
         Product prod = new Product("Testowy");
 
         entitymanager.persist(prod);
-        entitymanager.getTransaction().commit();
+//        entitymanager.getTransaction().commit();
 
         entitymanager.close();
         emfactory.close();
